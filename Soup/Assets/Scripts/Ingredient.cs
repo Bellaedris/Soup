86d277e96d0cd532dbc294cd741cf325e2520670
Florent Diet <diet.florent1@gmail.com>
2023-02-08 16:09:07 +0100

    over qqchose mettre shader arthur

--- Soup/Assets/Scripts/Ingredient.cs
+++ Soup/Assets/Scripts/Ingredient.cs
@@ -12,6 +12,16 @@ public class Ingredient : MonoBehaviour
         this.nom = nom;
         this.objet = objet;
     }
+
+    private void OnMouseOver() {
+        Debug.Log("Mouse is over " + nom);
+
+    }
+    void OnMouseExit()
+    {
+        //The mouse is no longer hovering over the GameObject so output this message each frame
+        Debug.Log("Mouse is no longer on " + nom);
+    }
 }
 
 
--- Soup/Assets/Scripts/MarketManager.cs
+++ Soup/Assets/Scripts/MarketManager.cs
@@ -43,7 +43,7 @@ public class MarketManager : MonoBehaviour
             }
         } 
     }
-
+    
 
     private void Update() {
 
