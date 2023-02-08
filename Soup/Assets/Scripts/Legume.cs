978670f58c72f909f05f0e9a1f8e5d7cce18a92e
p2105195 <Azqserdftygh7>
2023-02-08 14:10:28 +0100

    inventory UI functionning

--- Soup/Assets/Scripts/Legume.cs
+++ Soup/Assets/Scripts/Legume.cs
@@ -7,7 +7,7 @@ public class Legume : Ingredient
     public bool isMixed;
     public Color couleur;
 
-    public Legume(string nom, GameObject objet, bool isMixed, Color couleur) : base(nom, objet)
+    public Legume(string nom, Mesh objet, bool isMixed, Color couleur) : base(nom, objet)
     {
         this.isMixed = isMixed;
         this.couleur = couleur;
