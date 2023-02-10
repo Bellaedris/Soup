93685695e0657f4effdf85ecd8c4416471b47ab9
Melodie G <melodie.guerin@gmail.com>
2023-02-10 23:03:52 +0100

    Add the instanciation of the guest in the scene Dinner, the selection of the guest done

--- Soup/Assets/Scripts/DinnerManager.cs
+++ Soup/Assets/Scripts/DinnerManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class DinnerManager : MonoBehaviour
 {
+
+    public GameObject guestPrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +15,11 @@ public class DinnerManager : MonoBehaviour
         } else
         {
             Debug.Log("Guest : " + GameManager.instance.guest.characterName);
-        }   
-        
+            string guestName = GameManager.instance.guest.characterName;
+            guestPrefab = (GameObject)Resources.Load("prefab/Characters/"+guestName, typeof(GameObject));
+            guestPrefab.transform.localScale = Vector3.one;
+            Instantiate(guestPrefab, new Vector3(-20.2f, 0.5f, -6.2f), Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
