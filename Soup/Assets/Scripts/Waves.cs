a3c83a2ce16cd3ca3edebe79f75ca6daa9694b16
Arthur Pradier <arthur.pradier81@gmail.com>
2023-02-20 18:01:20 +0100

    Fixed performances issues on soup waves
    reading the normals of the mesh was calling a getter and creating extra garbage

--- Soup/Assets/Scripts/Waves.cs
+++ Soup/Assets/Scripts/Waves.cs
@@ -7,6 +7,7 @@ public class Waves : MonoBehaviour
 {
     private Mesh mesh;
     private Vector3[] vert;
+    private Vector3[] norm;
     Vector3[] newVert;
 
     // Start is called before the first frame update
@@ -14,6 +15,7 @@ public class Waves : MonoBehaviour
     {
         mesh = GetComponent<MeshFilter>().mesh;
         vert = mesh.vertices;
+        norm = mesh.normals;
         newVert = mesh.vertices;
     }
 
@@ -22,7 +24,7 @@ public class Waves : MonoBehaviour
     {
         for (int i = 0; i < vert.Length; i++)
         {
-            newVert[i] = vert[i] + mesh.normals[i] * WaveManager.instance.GetWaveLength(vert[i].x, vert[i].z);
+            newVert[i] = vert[i] + norm[i] * WaveManager.instance.GetWaveLength(vert[i].x, vert[i].z);
         }
 
         mesh.vertices = newVert;
