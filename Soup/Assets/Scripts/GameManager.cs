15200bb76181999ef0c8f0d7f9bf484c7895d8c0
Florent Diet <diet.florent1@gmail.com>
2023-02-21 16:36:39 +0100

    fin du merge de flo normalement

--- Soup/Assets/Scripts/GameManager.cs
+++ Soup/Assets/Scripts/GameManager.cs
@@ -5,10 +5,11 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Purchasing;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public ErrorNotificationController errorNotificationController;
 
     public int maxIngredientInventory;
     public string guest;
@@ -20,6 +21,8 @@ public class GameManager : MonoBehaviour
     public List<Ingredient> ingredientsSoup;
     public GameObject characterBook;
     public GameObject recipeBook;
+    public Text notificationText;
+    public Animator Transition;
 
     private void Awake() {
         if(instance!=null){
@@ -52,16 +55,23 @@ public class GameManager : MonoBehaviour
     }
 
     public void loadKitchenScene() 
-    {
-        if(guest.Equals(""))
+    {        
+        if(instance.guest == null || instance.guest.Equals(""))
         {
-            Debug.Log("Pick a guest please");
+            errorNotificationController.showNotification("Pick a guest please");
         } else
-        {
-            SceneManager.LoadScene(1);
+        {   
+            StartCoroutine(loadscene("KitchenUI")); 
         }
+        
+        
     }
-
+    
+    public IEnumerator loadscene(string scene){
+        Transition.SetTrigger("Start");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(scene);
+    }
     public void loadDinnerScene(Soup finishedSoup)
     {
         
@@ -179,4 +189,24 @@ public class GameManager : MonoBehaviour
         recipeBook.SetActive(!recipeBook.activeSelf);
     }
 
+    IEnumerator sendNotification(string text, int time){
+        notificationText.text = text;
+        yield return new WaitForSeconds(time);
+        notificationText.text = "";
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
